637a69533dcafdcec3a77fdca2ab3d2c9d877d7f
Site <68510239+dietilaminometilcumarin@users.noreply.github.com>
2023-02-11 04:26:54 +0200

    Update scripts_1.js

--- js/scripts_1.js
+++ js/scripts_1.js
@@ -7,5 +7,5 @@ function n(t,e,a){
 		var z = e.currentSrc.substring(e.currentSrc.lastIndexOf('/') + 1);
 		var a=t('input[data-url="'+z+'"]'),	n=a.attr("data-title"),i=a.attr("data-artist");
 		
-		alert(z);
+		//alert(z);
 	t(".mushometitle").html(n),t(".mushomeartist").html(i)}),!1)}}),t(".audio1 .mejs-prevtrack-button").addClass("mejs-cust1-button"),t(".audio1 .mejs-nexttrack-button").addClass("mejs-cust2-button")),(a=t(".audio2 audio")).length>0&&a.mediaelementplayer({features:["playpause","progress"],success:function(e){e.addEventListener("loadedmetadata",(function(){var a=Math.floor(e.duration/60),i=parseInt(e.duration)-60*a,r=n(a,"0",2)+"."+n(i,"0",2);t(this).parent().parent().parent().parent().parent().parent().find("span.duration").html(r)}))}}),(a=t(".audio3 audio")).length>0&&a.mediaelementplayer({features:["playpause"]}),(a=t(".audio4 audio")).length>0&&a.mediaelementplayer({features:["playpause","progress"]}),(a=t(".accordion")).length>0&&a.accordion({active:0,heightStyle:"content"}),(a=t(".animated-number")).length>0&&a.appear((function(){var e=t(this),a=e.text(),n=e.data("duration"),i=e.data("animation-delay");i||(i=0),e.text("0"),setTimeout((function(){e.animate({num:a},{duration:n,step:function(t){this.innerHTML=t.toFixed(0)}})}),i)})),(a=t(".owl-carousel-testimonials")).length>0&&a.owlCarousel({items:1,loop:!0,margin:30})}));
